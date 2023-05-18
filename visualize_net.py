83244fbedd20c883a173f6480bc5cc8b5db098fc
Ben Moore <benmoore@Bens-MacBook-Pro.local>
2023-05-18 09:45:03 -0500

    Added experiments with monoplex and multilayer networks.

--- visualize_net.py
+++ visualize_net.py
@@ -1,7 +1,7 @@
 from pymnet import *
 
 # Create a random graph with 10 nodes and a connection probability of 0.4 for each layer
-net = er(10, 3 * [0.4])
+net = er(3, [0.4, 0.4])
 
 # Draw the graph using the spring layout
 fig = draw(
